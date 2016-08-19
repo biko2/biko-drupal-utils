079e9e6f7d619a022a1b9cccf79fb1ea350be1d7
davgava <david.gallego@biko2.com>
2016-08-19 13:12:48 +0200

    url image style

--- src/Render/BikoRenderService.php
+++ src/Render/BikoRenderService.php
@@ -56,39 +56,11 @@ class BikoRenderService
     {
         $file = File::load($fid);
         if ($file) {
-            /*$variables = array(
-        'style_name' => $imageStyle,
-        'uri' => $file->getFileUri(),
-      );
-      // The image.factory service will check if our image is valid.
-      $image = \Drupal::service('image.factory')->get($file->getFileUri());
-            if ($image->isValid()) {
-                $variables['width'] = $image->getWidth();
-                $variables['height'] = $image->getHeight();
-            } else {
-                $variables['width'] = $variables['height'] = null;
-            }
-            $logo_build = [
-        '#theme' => 'image_style',
-        '#width' => $variables['width'],
-        '#height' => $variables['height'],
-        '#style_name' => $variables['style_name'],
-        '#uri' => $variables['uri'],
-
-      ];
-      // Add the file entity to the cache dependencies.
-      // This will clear our cache when this entity updates.
-      $renderer = \Drupal::service('renderer');
-            $renderer->addCacheableDependency($logo_build, $file);*/
             $style = entity_load('image_style', $imageStyle);
             return $style->buildUrl($file->getFileUri());
-      // Return the render array as block content.
-      /*return [
-        'logo' => $logo_build,
-      ];
         } else {
             // Image not found, return empty block.
-      return [];*/
+            return null;
         }
     }
 }
